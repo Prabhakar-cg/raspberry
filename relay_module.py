39afbee686789c86852f08a1d1054b77df290c7d
Prabha <justalearner@learning.com>
2024-09-24 15:16:07 +0400

    Relay working code

--- relay_module.py
+++ relay_module.py
@@ -2,21 +2,23 @@ import RPi.GPIO as GPIO
 
 import time
 
-Relay_Ch1 = 26
+Relay_Ch1 = 14
 
-Relay_Ch2 = 20
+Relay_Ch2 = 15
 
-Relay_Ch3 = 21
+Relay_Ch3 = 18
+
+Relay_Ch4 = 23
 
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(Relay_Ch1,GPIO.OUT)
-
 GPIO.setup(Relay_Ch2,GPIO.OUT)
-
 GPIO.setup(Relay_Ch3,GPIO.OUT)
+GPIO.setup(Relay_Ch4,GPIO.OUT)
+
 
 print("Setup The Relay Module is [success]")
 
@@ -62,6 +64,20 @@ try:
       GPIO.output(Relay_Ch3,GPIO.HIGH)
 
       print("Channel 3:The Common Contact is access to the Normal Closed Contact!\n")
+
+      time.sleep(0.5)
+
+#Control the Channel 4
+
+      GPIO.output(Relay_Ch4,GPIO.LOW)
+
+      print("Channel 4:The Common Contact is access to the Normal Open Contact!")
+
+      time.sleep(0.5)
+
+      GPIO.output(Relay_Ch4,GPIO.HIGH)
+
+      print("Channel 4:The Common Contact is access to the Normal Closed Contact!\n")
 
       time.sleep(0.5)
 
